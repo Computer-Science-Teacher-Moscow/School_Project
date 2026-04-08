d564ef3b20041bb62d2243d41f60c2a92076ceee
skirdovsa <skirdov-ser@yandex.ru>
2026-04-08 09:13:00 +0300

    Tasks 25

--- MCKO_10kl.py
+++ MCKO_10kl.py
@@ -11,15 +11,32 @@
 #         print(num)
 #         break
 
-from functools import lru_cache
-from fractions import Fraction
+# from functools import lru_cache
+# from fractions import Fraction
+#
+#
+# @lru_cache()
+# def f(n):
+#     if n == 1: return 1
+#     return n * f(n - 1)
+#
+#
+# for i in range(1, 2025): f(i)
+# print((Fraction(f(2025), 25) + f(2024)) / f(2023))
 
+# size = 1024 * 280
+# V = 280 * 2 ** 13
+# for i in range(1, 1000):
+#     if size * (i + 3) == V:
+#         print(2 ** i)
+#         break
 
-@lru_cache()
-def f(n):
-    if n == 1: return 1
-    return n * f(n - 1)
+# from itertools import product
+#
+# word = ''.join(sorted('ВЕСНА'))
+# for i, pr in enumerate(product(word, repeat = 4), 1):
+#     w = ''.join(pr)
+#     if 'Е' not in w and 'АА' not in w:
+#         print(i)
 
-
-for i in range(1, 2025): f(i)
-print((Fraction(f(2025), 25) + f(2024)) / f(2023))
+from ipaddress import ip_network, ip_address
